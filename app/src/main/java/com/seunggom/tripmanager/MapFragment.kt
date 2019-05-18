c0cf686d0295515eb460118e362feae56862a0c4
seunggom <47077179+seunggom@users.noreply.github.com>
2019-05-18 22:35:26 +0900

    use ZoomView

--- app/src/main/java/com/seunggom/tripmanager/MapFragment.kt
+++ app/src/main/java/com/seunggom/tripmanager/MapFragment.kt
@@ -1,12 +1,15 @@
 package com.seunggom.tripmanager
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.seunggom.tripmanager.ZoomView
+
+
+import kotlinx.android.synthetic.main.fragment_map.*
 
 
 class MapFragment : Fragment() {
@@ -14,12 +17,16 @@ class MapFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+
+
         return inflater.inflate(R.layout.fragment_map, container, false)
     }
+
 
     /*
         // TODO: Rename method, update argument and hook method into UI event
---
+++ app/src/main/java/com/seunggom/tripmanager/TripData.kt
@@ -0,0 +1,10 @@
+package com.seunggom.tripmanager
+
+import java.util.*
+
+class TripData {
+    lateinit var title : String
+    lateinit var startDate : Date
+    lateinit var endDate : Date
+
+}
