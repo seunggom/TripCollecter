a670724fe38721406089f8b13c0c74820502021e
seunggom <47077179+seunggom@users.noreply.github.com>
2019-06-03 19:04:11 +0900

    spinner 완성

--- app/src/main/java/com/seunggom/tripmanager/AddTripActivity.kt
+++ app/src/main/java/com/seunggom/tripmanager/AddTripActivity.kt
@@ -2,6 +2,7 @@ package com.seunggom.tripmanager
 
 import android.app.DatePickerDialog
 import android.content.Context
+import android.content.DialogInterface
 import android.database.Cursor
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -21,15 +22,14 @@ import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.AdapterView
 import kotlinx.android.synthetic.main.edit_region.*
+import kotlinx.android.synthetic.main.edit_region.view.*
 
 
 class AddTripActivity : AppCompatActivity() {
     var title: String? = null
 
 
-    var list = arrayListOf<addRegionData>(
-        addRegionData("전라남도 순천")
-    )
+    var list = arrayListOf<addRegionData>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -78,55 +78,63 @@ class AddTripActivity : AppCompatActivity() {
 
         addRegionBtn.setOnClickListener {
             var name1 : String? = null
+            var si_do_pos : Int = 0
             var name2 : String? = null
             var imageUrl: Set<String>? = null
 
 
             val builder = AlertDialog.Builder(this)
+
+            builder.setTitle("지역 & 사진 추가하기")
+                .setPositiveButton("추가") {dialog, which ->
+                    list.add(addRegionData(name1, name2))
+                    mAdapter.notifyItemInserted(0) }
+                .setNeutralButton("취소", null)
+                .create()
+
             val view = LayoutInflater.from(this).inflate(R.layout.edit_region, null, false)
             builder.setView(view)
 
-            val sp1 = findViewById(R.id.spinner1) as Spinner
-            val sp1Adapter = ArrayAdapter.createFromResource(this, R.array.si_do, R.layout.edit_region)
-            sp1.adapter = sp1Adapter
-            sp1.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-                    name1 = sp1.selectedItem.toString()
-                }
+            val sp1Adapter = ArrayAdapter.createFromResource(this, R.array.si_do, android.R.layout.simple_spinner_dropdown_item)
+            view.spinner1.adapter = sp1Adapter
+            val sp2Adapter = ArrayAdapter.createFromResource(this, R.array.si_do_0, android.R.layout.simple_spinner_dropdown_item)
+            view.spinner2.adapter = sp2Adapter
 
+            view.spinner1.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
                 override fun onNothingSelected(parent: AdapterView<*>?) {
-                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
                 }
+                override fun onItemSelected(parent: AdapterView<*>, v: View?, position: Int, id: Long) {
+                    name1 = view.spinner1.getItemAtPosition(position).toString()
 
+                    when(position) {
+                        0 -> si_do_pos = R.array.si_do_0
+                        1 -> si_do_pos = R.array.si_do_1
+                        2 -> si_do_pos = R.array.si_do_2
+                        3 -> si_do_pos = R.array.si_do_3
+                        4 -> si_do_pos = R.array.si_do_4
+                        5 -> si_do_pos = R.array.si_do_5
+                        6 -> si_do_pos = R.array.si_do_6
+                        7 -> si_do_pos = R.array.si_do_7
+                        8 -> si_do_pos = R.array.si_do_8
+                        9 -> si_do_pos = R.array.si_do_9
+                        10 -> si_do_pos = R.array.si_do_10
+                    }
+
+                    view.spinner2.adapter = ArrayAdapter.createFromResource(this@AddTripActivity, si_do_pos, android.R.layout.simple_spinner_dropdown_item)
+
+                }
             }
 
-            val sp2 = findViewById(R.id.spinner2) as Spinner
-            val sp2Adapter = ArrayAdapter.createFromResource(this, R.array.si_do, R.layout.edit_region)
-            sp1.adapter = sp2Adapter
-            sp1.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-                    name2 = sp2.selectedItem.toString()
-                }
-
+            view.spinner2.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
                 override fun onNothingSelected(parent: AdapterView<*>?) {
-                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
                 }
 
+                override fun onItemSelected(parent: AdapterView<*>, v: View?, position: Int, id: Long) {
+                    name2 = view.spinner2.getItemAtPosition(position).toString()
+                }
             }
 
-            val dialog = builder.create()
-
-            addFinishBtn.setOnClickListener {
-                list.add(addRegionData(name1, name2))
-
-                mAdapter.notifyItemInserted(0)
-
-                dialog.dismiss()
-            }
-
-            dialog.show()
+            builder.show()
         }
 
 
